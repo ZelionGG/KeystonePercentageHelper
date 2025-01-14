c3dcc206eda16e765906b6832cc580dce607add4
ZelionGG <contact@zeliongg.com>
2025-01-14 01:54:09 +0100

    Localization updates.

--- locales/ruRU.lua
+++ locales/ruRU.lua
@@ -9,6 +9,9 @@ local L = AceLocale:NewLocale(AddonName, "ruRU", true, false); ---@class XIV_Dat
 -- Dungeons Group
 L["DUNGEONS"] = "Current Season"
 L["CURRENT_SEASON"] = "Current Season"
+L["CURRENT_SEASON_DUNGEONS"] = "Current Season Dungeons"
+L["NEXT_SEASON"] = "Next Season"
+L["NEXT_SEASON_DUNGEONS"] = "Next Season Dungeons"
 L["EXPANSION_DF"] = "Dragonflight"
 L["EXPANSION_CATA"] = "Cataclysm"
 L["EXPANSION_WW"] = "The War Within"
@@ -30,11 +33,44 @@ L["CoT_BOSS2"] = "Fangs of the Queen"
 L["CoT_BOSS3"] = "The Coaglamation"
 L["CoT_BOSS4"] = "Izo, the Grand Splicer"
 
+-- Cinderbrew Meadery
+L["CBM"] = "Cinderbrew Meadery"
+L["CBM_BOSS1"] = "Brew Master Aldryr"
+L["CBM_BOSS2"] = "I'pa"
+L["CBM_BOSS3"] = "Benk Buzzbee"
+L["CBM_BOSS4"] = "Goldie Baronbottom"
+
+-- Darkflame Cleft
+L["DFC"] = "Darkflame Cleft"
+L["DFC_BOSS1"] = "Ol' Waxbeard"
+L["DFC_BOSS2"] = "Blazikon"
+L["DFC_BOSS3"] = "The Candle King"
+L["DFC_BOSS4"] = "The Darkness"
+
+-- Operation: Floodgate
+L["OFG"] = "Operation: Floodgate"
+L["OFG_BOSS1"] = "Big M.O.M.M.A."
+L["OFG_BOSS2"] = "Demolition Duo"
+L["OFG_BOSS3"] = "Swampface"
+L["OFG_BOSS4"] = "Geezle Gigazap"
+
+-- Priory of the Sacred Flames
+L["PotSF"] = "Priory of the Sacred Flames"
+L["PotSF_BOSS1"] = "Captain Dailcry"
+L["PotSF_BOSS2"] = "Baron Braunpyke"
+L["PotSF_BOSS3"] = "Prioress Murrpray"
+
 -- The Dawnbreaker
 L["TDB"] = "The Dawnbreaker"
 L["TDB_BOSS1"] = "Speaker Shadowcrown"
 L["TDB_BOSS2"] = "Anub'ikkaj"
 L["TDB_BOSS3"] = "Rasha'nan"
+
+-- The Rookery
+L["TR"] = "The Rookery"
+L["TR_BOSS1"] = "Kyrioss"
+L["TR_BOSS2"] = "Stormguard Gorren"
+L["TR_BOSS3"] = "Voidstone Monstrosity"
 
 -- The Stonevault
 L["TSV"] = "The Stonevault"
@@ -50,13 +86,6 @@ L["MoTS_BOSS1"] = "Ingra Maloch"
 L["MoTS_BOSS2"] = "Mistcaller"
 L["MoTS_BOSS3"] = "Tred'ova"
 
--- The Necrotic Wake
-L["NW"] = "The Necrotic Wake"
-L["NW_BOSS1"] = "Blightbone"
-L["NW_BOSS2"] = "Amarth"
-L["NW_BOSS3"] = "Surgeon Stitchflesh"
-L["NW_BOSS4"] = "Nalthor the Rimebinder"
-
 --Theater of Pain
 L["ToP"] = "Theater of Pain"
 L["ToP_BOSS1"] = "An Affront of Challengers"
@@ -65,7 +94,22 @@ L["ToP_BOSS3"] = "Xav the Unfallen"
 L["ToP_BOSS4"] = "Kul'tharok"
 L["ToP_BOSS5"] = "Mordretha, the Endless Empress"
 
+-- The Necrotic Wake
+L["NW"] = "The Necrotic Wake"
+L["NW_BOSS1"] = "Blightbone"
+L["NW_BOSS2"] = "Amarth"
+L["NW_BOSS3"] = "Surgeon Stitchflesh"
+L["NW_BOSS4"] = "Nalthor the Rimebinder"
+
+
 -- Battle for Azeroth
+-- Operation: Mechagon - Workshop
+L["OMGW"] = "Operation: Mechagon - Workshop"
+L["OMGW_BOSS1"] = "Tussle Tonks"
+L["OMGW_BOSS2"] = "K.U.-J.0."
+L["OMGW_BOSS3"] = "Machinist's Garden"
+L["OMGW_BOSS4"] = "King Mechagon"
+
 -- Siege of Boralus
 L["SoB"] = "Siege of Boralus"
 L["SoB_BOSS1"] = "Chopper Redhook"
@@ -73,13 +117,14 @@ L["SoB_BOSS2"] = "Dread Captain Lockwood"
 L["SoB_BOSS3"] = "Hadal Darkfathom"
 L["SoB_BOSS4"] = "Viq'Goth"
 
--- Legion
 -- The MOTHERLODE!!
 L["TML"] = "The MOTHERLODE!!"
 L["TML_BOSS1"] = "Coin-Operated Crowd Pummeler"
 L["TML_BOSS2"] = "Azerokk"
 L["TML_BOSS3"] = "Rixxa Fluxflame"
 L["TML_BOSS4"] = "Mogul Razdunk"
+
+-- Legion
 
 -- Cataclysm
 -- Grim Batol
@@ -92,6 +137,13 @@ L["GB_BOSS4"] = "Erudax, the Duke of Below"
 -- UI Strings
 L["OPTIONS"] = "Options"
 L["GENERAL_SETTINGS"] = "General Settings"
+L["Changelog"] = "Changelog"
+L["Version"] = "Version"
+L["Important"] = "Important"
+L["New"] = "New"
+L["Bugfixes"] = "Bug fixes"
+L["Improvment"] = "Improvements"
+L["%month%-%day%-%year%"] = "%year%-%month%-%day%"
 L["DEFAULT_PERCENTAGES"] = "Default percentages"
 L["ADVANCED_SETTINGS"] = "Custom routes"
 L["TANK_GROUP_HEADER"] = "Boss Percentages"
@@ -117,9 +169,17 @@ L["FINISHED"] = "Finished"
 L["MISSING"] = "Missing"
 L["GENERAL"] = "General"
 L["ANCHOR_POSITION"] = "Anchor Position"
+L["VALIDATE"] = "Validate"
+L["CANCEL"] = "Cancel"
+L["POSITION"] = "Position"
 L["TOP"] = "Top"
-L["CENTER"] = "Center"
+L["TOPLEFT"] = "Top Left"
+L["TOPRIGHT"] = "Top Right"
 L["BOTTOM"] = "Bottom"
+L["BOTTOMLEFT"] = "Bottom Left"
+L["BOTTOMRIGHT"] = "Bottom Right"
+L["LEFT"] = "Left"
+L["RIGHT"] = "Right"
 L["X_OFFSET"] = "X Offset"
 L["Y_OFFSET"] = "Y Offset"
 L["SHOW_ANCHOR"] = "Show Positioning Anchor"
@@ -130,3 +190,4 @@ L["RESET_DUNGEON_CONFIRM"] = "Are you sure you want to reset all boss percentage
 L["RESET_ALL_DUNGEONS"] = "Reset All Dungeons"
 L["RESET_ALL_DUNGEONS_DESC"] = "Reset all dungeons to their default values"
 L["RESET_ALL_DUNGEONS_CONFIRM"] = "Are you sure you want to reset all dungeons to their default values?"
+L["NEW_SEASON_RESET_PROMPT"] = "A new Mythic+ season has started. Would you like to reset all dungeon values to their defaults?"
