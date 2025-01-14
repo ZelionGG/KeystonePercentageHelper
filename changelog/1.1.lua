02ecd6dda7e2a47726951d89e56c4a5ed0ca86bd
ZelionGG <contact@zeliongg.com>
2025-01-14 01:49:13 +0100

    Changelog system implementation.

---
+++ changelog/1.1.lua
@@ -0,0 +1,47 @@
+local AddOnName, KeystonePercentageHelper = ...;
+
+KeystonePercentageHelper.Changelog[1100] = {
+    version_string = "1.1",
+    release_date = "2025/01/14",
+    important = {
+        ["zhCN"] = {},
+        ["zhTW"] = {},
+        ["enUS"] = {
+            "Fixed incorrect season start date",
+        },
+        ["frFR"] = {},
+        ["koKR"] = {},
+        ["ruRU"] = {}
+    },
+    new = {
+        ["zhCN"] = {},
+        ["zhTW"] = {},
+        ["enUS"] = {
+            "Added a fullscreen overlay when positioning the frame",
+            "Added Cancel button and ESC key support for frame positioning",
+            "Added automatic hiding of positioning UI during combat",
+        },
+        ["frFR"] = {},
+        ["koKR"] = {},
+        ["ruRU"] = {}
+    },
+    bugfix = {
+        ["zhCN"] = {},
+        ["zhTW"] = {},
+        ["enUS"] = {},
+        ["frFR"] = {},
+        ["koKR"] = {},
+        ["ruRU"] = {}
+    },
+    improvment = {
+        ["zhCN"] = {},
+        ["zhTW"] = {},
+        ["enUS"] = {
+            "Improved frame positioning interface with better visibility",
+            "Added changelog system",
+        },
+        ["frFR"] = {},
+        ["koKR"] = {},
+        ["ruRU"] = {}
+    }
+}
