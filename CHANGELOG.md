752aeb2d9bd651f8f72785ac5740b579688fe076
ZelionGG <contact@zeliongg.com>
2025-03-21 20:16:42 +0100

    CHANGELOG update.

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,9 +1,7 @@
 # Keystone Percentage Helper
 
-## [1.4.1](https://github.com/ZelionGG/KeystonePercentageHelper/tree/1.4.1) (2025-03-19)
+## [1.4.2](https://github.com/ZelionGG/KeystonePercentageHelper/tree/1.4.2) (2025-03-21)
 
-[Full Changelog](https://github.com/ZelionGG/KeystonePercentageHelper/compare/1.4...1.4.1) [Previous Releases](https://github.com/ZelionGG/KeystonePercentageHelper/releases)
+[Full Changelog](https://github.com/ZelionGG/KeystonePercentageHelper/compare/1.4.1...1.4.2) [Previous Releases](https://github.com/ZelionGG/KeystonePercentageHelper/releases)
 
-- Fixed negative percentages happening in dungeons (for example Operation: Floodgate) where a boss percentage required was lower than the previous boss, it will now show "Done" as expected.
-- Added locale files for "Done" and "Finished" strings.
-- Fixed LibSharedMedia loading order (thanks scizzydo).
+- Tentative change to the "Done" and "Finished" strings and addition of a new "Section done" string (any feedback is welcome).
---
+++ changelog/1.4.2.lua
@@ -0,0 +1,60 @@
+local AddOnName, KeystonePercentageHelper = ...;
+
+KeystonePercentageHelper.Changelog[1420] = {
+    version_string = "1.4.2",
+    release_date = "2025/03/21",
+    important = {
+        ["zhCN"] = {},
+        ["zhTW"] = {},
+        ["enUS"] = {},
+        ["frFR"] = {},
+        ["koKR"] = {},
+        ["ruRU"] = {},
+        ["deDE"] = {},
+        ["esES"] = {},
+        ["esMX"] = {},
+        ["itIT"] = {},
+        ["ptBR"] = {}
+    },
+    new = {
+        ["zhCN"] = {},
+        ["zhTW"] = {},
+        ["enUS"] = {},
+        ["frFR"] = {},
+        ["koKR"] = {},
+        ["ruRU"] = {},
+        ["deDE"] = {},
+        ["esES"] = {},
+        ["esMX"] = {},
+        ["itIT"] = {},
+        ["ptBR"] = {}
+    },
+    bugfix = {
+        ["zhCN"] = {},
+        ["zhTW"] = {},
+        ["enUS"] = {},
+        ["frFR"] = {},
+        ["koKR"] = {},
+        ["ruRU"] = {},
+        ["deDE"] = {},
+        ["esES"] = {},
+        ["esMX"] = {},
+        ["itIT"] = {},
+        ["ptBR"] = {}
+    },
+    improvment = {
+        ["zhCN"] = {},
+        ["zhTW"] = {},
+        ["enUS"] = {
+            "Tentative change to the \"Done\" and \"Finished\" strings and addition of a new \"Section done\" string (any feedback is welcome)."
+        },
+        ["frFR"] = {},
+        ["koKR"] = {},
+        ["ruRU"] = {},
+        ["deDE"] = {},
+        ["esES"] = {},
+        ["esMX"] = {},
+        ["itIT"] = {},
+        ["ptBR"] = {}
+    }
+}
