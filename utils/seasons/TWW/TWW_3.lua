02b1fcd4a4050f007e17145c775a5a2193f3229d
ZelionGG <contact@zeliongg.com>
2025-06-18 23:35:58 +0200

    Added season 3 dungeons.

---
+++ utils/seasons/TWW/TWW_3.lua
@@ -0,0 +1,15 @@
+local AddOnName, KeystonePercentageHelper = ...
+
+-- Define which dungeons are in the current season
+KeystonePercentageHelper.TWW_3_DUNGEONS = {
+    -- War Within dungeons
+    [503] = true, -- AKCE
+    [499] = true, -- PotSF
+    [505] = true, -- TDB
+    [525] = true, -- OFG (Operation: Floodgate)
+    --[526] = true, -- Eco-dome Al'dani (TBD)
+    -- Shadowlands dungeons
+    [391] = true, -- Tazavesh: Streets of Wonder
+    [392] = true, -- Tazavesh: So'leah's Gambit
+    [378] = true, -- Halls of Atonement
+}
