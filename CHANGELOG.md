5e28679a52dcc38a320edd0162767cde5831030c
ZelionGG <contact@zeliongg.com>
2025-08-27 10:18:53 +0200

    CHANGELOGs update.

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,10 +1,13 @@
 # Keystone Percentage Helper
 
-## [1.7.1](https://github.com/ZelionGG/KeystonePercentageHelper/releases/tag/1.7.1) (2025-08-11)
+## [2.0](https://github.com/ZelionGG/KeystonePercentageHelper/releases/tag/2.0) (2025-08-27)
 
-[Full Changelog](https://github.com/ZelionGG/KeystonePercentageHelper/compare/1.7...1.7.1) [Previous Releases](https://github.com/ZelionGG/KeystonePercentageHelper/releases)
+[Full Changelog](https://github.com/ZelionGG/KeystonePercentageHelper/compare/1.7.1...2.0) [Previous Releases](https://github.com/ZelionGG/KeystonePercentageHelper/releases)
 
-- **Season 3** is now fully operational, Eco-Dome Al'dani should show the correct name and icon. Enjoy Season 3 preparation!
-- Korean translation updated, thank you again **BlueSea-jun**.
-- Russian translation updated, thank you **Hollicsh**.
+- [#58](https://github.com/ZelionGG/KeystonePercentageHelper/issues/58) - Increased the maximum font size for the percentage text to 64
+- [#57](https://github.com/ZelionGG/KeystonePercentageHelper/issues/57) - Fixed the export feature
+- **NEW** - Added an integration with Mythic Dungeon Tools :
+    - Show the percentage given by a mob next to its nameplate
+    - Show an indicator for mobs that are included in your MDT route
+
 
---
+++ utils/changelog/2.0.lua
@@ -0,0 +1,74 @@
+local AddOnName, KeystonePercentageHelper = ...;
+
+KeystonePercentageHelper.Changelog[2000] = {
+    version_string = "2.0",
+    release_date = "2025/08/27",
+    important = {
+        ["zhCN"] = {},
+        ["zhTW"] = {},
+        ["enUS"] = {},
+        ["frFR"] = {},
+        ["koKR"] = {},
+        ["ruRU"] = {},
+        ["deDE"] = {},
+        ["esES"] = {},
+        ["esMX"] = {},
+        ["itIT"] = {},
+        ["ptBR"] = {}
+    },
+    new = {
+        ["zhCN"] = {},
+        ["zhTW"] = {},
+        ["enUS"] = {
+            "Added an integration with Mythic Dungeon Tools :\n"
+                .. "\t- Show the percentage given by a mob next to its nameplate.\n"
+                .. "\t- Show an indicator for mobs that are included in your MDT route."
+        },
+        ["frFR"] = {
+            "Ajout d'une intégration avec Mythic Dungeon Tools :\n"
+                .. "\t- Affiche le pourcentage donné par un monstre à côté de son nom.\n"
+                .. "\t- Affiche un indicateur pour les monstres qui sont inclus dans votre route MDT."
+        },
+        ["koKR"] = {},
+        ["ruRU"] = {},
+        ["deDE"] = {},
+        ["esES"] = {},
+        ["esMX"] = {},
+        ["itIT"] = {},
+        ["ptBR"] = {}
+    },
+    bugfix = {
+        ["zhCN"] = {},
+        ["zhTW"] = {},
+        ["enUS"] = {
+            "Fixed the export string not being generated."
+        },
+        ["frFR"] = {
+            "Correction du problème de génération de la chaîne d'export."
+        },
+        ["koKR"] = {},
+        ["ruRU"] = {},
+        ["deDE"] = {},
+        ["esES"] = {},
+        ["esMX"] = {},
+        ["itIT"] = {},
+        ["ptBR"] = {}
+    },
+    improvment = {
+        ["zhCN"] = {},
+        ["zhTW"] = {},
+        ["enUS"] = {
+            "Increased the maximum font size for the required percentage text to 64"
+        },
+        ["frFR"] = {
+            "Taille maximale du texte du pourcentage requis augmentée à 64"
+        },
+        ["koKR"] = {},
+        ["ruRU"] = {},
+        ["deDE"] = {},
+        ["esES"] = {},
+        ["esMX"] = {},
+        ["itIT"] = {},
+        ["ptBR"] = {}
+    }
+}
