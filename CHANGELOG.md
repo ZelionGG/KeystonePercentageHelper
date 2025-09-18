857f2a0568220f0609e68463ef478090fc4b75fc
ZelionGG <contact@zeliongg.com>
2025-09-18 23:06:29 +0200

    CHANGELOGs update.

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,11 +1,11 @@
 # Keystone Percentage Helper
 
-## [2.0.1](https://github.com/ZelionGG/KeystonePercentageHelper/releases/tag/2.0.1) (2025-09-12)
+## [2.1](https://github.com/ZelionGG/KeystonePercentageHelper/releases/tag/2.1) (2025-09-18)
 
-[Full Changelog](https://github.com/ZelionGG/KeystonePercentageHelper/compare/2.0...2.0.1) [Previous Releases](https://github.com/ZelionGG/KeystonePercentageHelper/releases)
+[Full Changelog](https://github.com/ZelionGG/KeystonePercentageHelper/compare/2.0.1...2.1) [Previous Releases](https://github.com/ZelionGG/KeystonePercentageHelper/releases)
 
-- **Halls of Atonement** default route got updated with this week's rework.
+- Added new options to show **Current percentage** and **Pull percentage** (Requires MDT) alongside the actual required percentage display.
+- Added new option to switch between percentage and count-based display.
 - Russian translation updated, thank you **Hollicsh**.
-- German translation updated, thank you **KazumaKuma**.
 
 
---
+++ utils/changelog/2.1.lua
@@ -0,0 +1,68 @@
+local AddOnName, KeystonePercentageHelper = ...;
+
+KeystonePercentageHelper.Changelog[2100] = {
+    version_string = "2.1",
+    release_date = "2025/09/18",
+    important = {
+        ["zhCN"] = {},
+        ["zhTW"] = {},
+        ["enUS"] = {},
+        ["frFR"] = {},
+        ["koKR"] = {},
+        ["ruRU"] = {},
+        ["deDE"] = {},
+        ["esES"] = {},
+        ["esMX"] = {},
+        ["itIT"] = {},
+        ["ptBR"] = {}
+    },
+    new = {
+        ["zhCN"] = {},
+        ["zhTW"] = {},
+        ["enUS"] = {
+            "Added new options to show [Current percentage] and [Pull percentage] (Requires MDT) alongside the actual required percentage display.",
+            "Added new option to switch between [percentage] and [count-based] display."
+        },
+        ["frFR"] = {
+            "Ajout de nouvelles options pour afficher [Pourcentage actuel] et [Pourcentage du pull] (Requiert MDT) à côté du pourcentage réel requis.",
+            "Ajout d'une option pour basculer entre l'affichage en [pourcentage] et en [compte]."
+        },
+        ["koKR"] = {},
+        ["ruRU"] = {},
+        ["deDE"] = {},
+        ["esES"] = {},
+        ["esMX"] = {},
+        ["itIT"] = {},
+        ["ptBR"] = {}
+    },
+    bugfix = {
+        ["zhCN"] = {},
+        ["zhTW"] = {},
+        ["enUS"] = {},
+        ["frFR"] = {},
+        ["koKR"] = {},
+        ["ruRU"] = {},
+        ["deDE"] = {},
+        ["esES"] = {},
+        ["esMX"] = {},
+        ["itIT"] = {},
+        ["ptBR"] = {}
+    },
+    improvment = {
+        ["zhCN"] = {},
+        ["zhTW"] = {},
+        ["enUS"] = {
+            "Russian translation updated, thank you [Hollicsh]."
+        },
+        ["frFR"] = {
+            "Traduction russe mise à jour, merci à [Hollicsh]."
+        },
+        ["koKR"] = {},
+        ["ruRU"] = {},
+        ["deDE"] = {},
+        ["esES"] = {},
+        ["esMX"] = {},
+        ["itIT"] = {},
+        ["ptBR"] = {}
+    }
+}
