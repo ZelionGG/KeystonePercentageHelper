1194ca7659d98ebb319b9df3dc455082b97221f5
ZelionGG <contact@zeliongg.com>
2025-10-16 22:28:43 +0200

    CHANGELOGs update.

---
+++ KeystonePolaris/utils/changelog/3.0.1.lua
@@ -0,0 +1,86 @@
+local AddOnName, KeystonePolaris = ...;
+
+KeystonePolaris.Changelog[3001] = {
+    version_string = "3.0.1",
+    release_date = "2025/10/16",
+    important = {
+        ["zhCN"] = {},
+        ["zhTW"] = {},
+        ["enUS"] = {
+            "|cff40e0d0A NEW NAME FOR A BIGGER VISION|r\n\n"
+            .. "[Keystone Percentage Helper] becomes [Keystone Polaris] — a bold step that reflects the Mythic+ Swiss-army-knife DNA I imagined and built with the community. \n\n"
+            .. "Your settings are migrated automatically (no action needed), if you happen to lose your settings, please report it and copy the [KeystonePercentageHelper.lua] in your [WTF/Account/YourAccountName/SavedVariables] folder to a new [KeystonePolaris.lua] file to get your settings back (close the game before doing that). \n\n"
+            .. "New name. Same speed. Bigger ambitions. Follow the star — Polaris. [#68]",
+            "TOC updated for PTR [11.2.7].",
+        },
+        ["frFR"] = {
+            "|cff40e0d0UN NOUVEAU NOM POUR UNE PLUS GRANDE VISION|r \n\n"
+            .. "[Keystone Percentage Helper] devient [Keystone Polaris] — une étape ambitieuse qui reflète l'ADN de couteau suisse Mythique+ que j'ai voulu, imaginé et construit avec la communauté. \n\n"
+            .. "Vos paramètres sont migrés automatiquement (aucune action requise), si vous veniez à perdre vos paramètres, n'hésitez pas à le signaler sur GitHub, et, en solution alternative, vous pouvez copier le [KeystonePercentageHelper.lua] dans votre dossier [WTF/Account/YourAccountName/SavedVariables] dans un nouveau fichier [KeystonePolaris.lua] (fermez le jeu avant de le faire). \n\n"
+            .. "Nouveau nom. Même énergie. Plus d’ambition. Suivez l’étoile — Polaris. [#68]",
+            "Mise à jour du TOC pour le RPT [11.2.7].",
+        },
+        ["koKR"] = {},
+        ["ruRU"] = {
+            "|cff40e0d0НОВОЕ ИМЯ ДЛЯ БОЛЬШЕГО ВИДЕНИЯ|r\n\n"
+            .. "[Keystone Percentage Helper] становится [Keystone Polaris] - смелый шаг, отражающий ДНК швейцарского армейского ножа M+, которую я придумал и воплотил вместе с сообществом. \n\n"
+            .. "Ваши настройки переносятся автоматически (никаких действий не требуется). Если Вы случайно потеряли свои настройки, сообщите об этом и скопируйте файл [KeystonePercentageHelper.lua] из папки [WTF/Account/ВашеИмяАккаунта/SavedVariables] в новый файл [KeystonePolaris.lua], чтобы вернуть настройки (перед этим закройте игру). \n\n"
+            .. "Новое имя. Та же скорость. Большие амбиции. Следуй за звездой - Полярис. [#68]",
+            "Обновление файла TOC для патча [11.2.7].",
+        },
+        ["deDE"] = {},
+        ["esES"] = {},
+        ["esMX"] = {},
+        ["itIT"] = {},
+        ["ptBR"] = {}
+    },
+    new = {
+        ["zhCN"] = {},
+        ["zhTW"] = {},
+        ["enUS"] = {},
+        ["frFR"] = {},
+        ["koKR"] = {},
+        ["ruRU"] = {},
+        ["deDE"] = {},
+        ["esES"] = {},
+        ["esMX"] = {},
+        ["itIT"] = {},
+        ["ptBR"] = {}
+    },
+    bugfix = {
+        ["zhCN"] = {},
+        ["zhTW"] = {},
+        ["enUS"] = {},
+        ["frFR"] = {},
+        ["koKR"] = {},
+        ["ruRU"] = {},
+        ["deDE"] = {},
+        ["esES"] = {},
+        ["esMX"] = {},
+        ["itIT"] = {},
+        ["ptBR"] = {}
+    },
+    improvment = {
+        ["zhCN"] = {},
+        ["zhTW"] = {},
+        ["enUS"] = {
+            "Russian translation updated, thank you [Hollicsh].",
+            "Korean translation updated, thank you [BlueSea-jun]."
+
+        },
+        ["frFR"] = {
+            "Traduction russe mise à jour, merci à [Hollicsh].",
+            "Traduction coréenne mise à jour, merci à [BlueSea-jun]."
+        },
+        ["koKR"] = {},
+        ["ruRU"] = {
+            "Русский перевод обновлен, спасибо [Hollicsh].",
+            "Корейский перевод обновлен, спасибо [BlueSea-jun]."
+        },
+        ["deDE"] = {},
+        ["esES"] = {},
+        ["esMX"] = {},
+        ["itIT"] = {},
+        ["ptBR"] = {}
+    }
+}
